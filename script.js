66870d38da6bd2242fe4fe2b44b909af8493ba46
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-19 15:51:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ function updateText() {
+      // Get the <p> element by its id
+      var statusParagraph = document.getElementById('status');
+
+      // Create a new h1 element
+      var h1Element = document.createElement('h1');
+      
+      // Set the text content of the h1 element
+      h1Element.textContent = 'Entered Metaverse';
+
+      // Replace the existing <p> element with the new h1 element
+      statusParagraph.parentNode.replaceChild(h1Element, statusParagraph);
+    }
